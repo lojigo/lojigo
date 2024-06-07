b2851ddcc96be17bed6907ce7342abc823dec981
lojigo <agkhub@gmail.com>
2024-06-07 20:48:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Henüz belirli bir JavaScript fonksiyonu yok. Gelecekte dinamik özellikler eklemek için bu dosyayı kullanabilirsiniz.
+document.addEventListener('DOMContentLoaded', function() {
+    // Örneğin, butonlara tıklanıldığında ekstra animasyonlar ekleyebilirsiniz.
+});
